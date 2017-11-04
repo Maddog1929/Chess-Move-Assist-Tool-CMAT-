d71a30cd8dd9350057a3d2f04e8e09fefb8b2c82
Maddog1929 <cav4321@gmail.com>
2017-11-04 17:51:26 -0400

    Finished inspector/parser
    Finished parser and inspector. Added isLegal function to inspector which also checks for ambiguity.

--- Chess Move Assist Tool/Chess Move Assist Tool/Chess Move Assist Tool.cpp
+++ Chess Move Assist Tool/Chess Move Assist Tool/Chess Move Assist Tool.cpp
@@ -8,7 +8,7 @@ using namespace std;
 class Board
 {
 public:
-	int pieces[8][8] = {
+	/*int pieces[8][8] = {
 		{ 4, 2, 3, 5, 6, 3, 2, 4 } ,
 		{ 1, 1, 1, 1, 1, 1, 1, 1 } ,
 		{ 0, 0, 0, 0, 0, 0, 0, 0 } ,
@@ -18,13 +18,37 @@ public:
 		{ 11, 11, 11, 11, 11, 11, 11, 11 } ,
 		{ 14, 12, 13, 15, 16, 13, 12, 14 } ,
 	};
-	
+	*/
+
+	int pieces[8][8] = { // test board
+		{ 1, 1, 1, 0, 0, 0, 0, 0 } ,
+		{ 0, 11, 0, 0, 0, 0, 0, 0 } ,
+		{11, 11, 11, 0, 0, 0, 0, 0 } ,
+		{ 0, 0, 0, 0, 0, 0, 0, 0 } ,
+		{ 0, 0, 0, 0, 0, 0, 0, 0 } ,
+		{ 0, 0, 0, 0, 0, 0, 0, 0 } ,
+		{ 0, 0, 0, 0, 0, 0, 0, 0 } ,
+		{ 0, 0, 0, 0, 0, 0, 0, 0 } ,
+	};
 	bool whiteTurn = true;
 
-	void move(int x, int y, int x2, int y2)
+	void move(int y, int x, int y2, int x2)
 	{
 		pieces[x2 - 1][y2 - 1] = pieces[x - 1][y - 1];
 		pieces[x - 1][y - 1] = 0;
+	}
+
+	int get(int x, int y)
+	{
+		if( x < 0 || x > 8)
+		{
+			return 0;
+		}
+		if ( y < 0 || y > 8)
+		{
+			return 0;
+		}
+		return pieces[y][x];
 	}
 
 	void endTurn()
@@ -36,6 +60,429 @@ public:
 class Inspector
 {
 public:
+	Board board;
+	void updateBoard(Board bd)
+	{
+		board = bd;
+	}
+	int isLegal(vector<int> move)
+	{
+		if (move[0] == 1)
+		{
+			switch (move[1])
+			{
+			case(6) : return false; break;
+			case(5) :
+				{
+					int file = move[2]-1;
+					int rank = move[3]-1;
+					int queen = false;
+					int block = false;
+					int queens = 0;
+					cout << "Looking for queens" << endl;
+					for (int i = 0; i < 9; i++) //left to right check
+					{
+						if (i == file)
+						{
+							queen = false;
+							continue;
+						}
+						if (i < file) 
+						{
+							//check for queen and if it has l.o.s to posistion
+							if (board.get(i, rank) == ((board.whiteTurn) ? 5 : 15))
+							{
+								queen = true;
+								queens++;
+							}
+							else if (board.get(i, rank) != 0 && queen == true)
+							{
+								queen == false;
+								queens--;
+							
+							}
+						}
+						else
+						{
+							if (board.get(i, rank) == ((board.whiteTurn) ? 5 : 15))
+							{
+								queens++;
+								break;
+							}
+							else if (board.get(i, rank) != 0)
+							{
+								break;
+							}
+						}
+					}//left to right check
+					cout << "Left to right done. " << queens << " queens" << endl;
+					for (int i = 0; i < 9; i++) //bottom to top check
+					{
+						if (i == rank)
+						{
+							queen = false;
+							continue;
+						}
+						if (i < rank)
+						{
+							//check for queen and if it has l.o.s to posistion
+							if (board.get(file, i) == ((board.whiteTurn) ? 5 : 15))
+							{
+								queen = true;
+								queens++;
+							}
+							else if (board.get(file, i) != 0 && queen == true)
+							{
+								queen == false;
+								queens--;
+
+							}
+						}
+						else
+						{
+							if (board.get(file, i) == ((board.whiteTurn) ? 5 : 15))
+							{
+								queens++;
+								break;
+							}
+							else if (board.get(file, i) != 0)
+							{
+								break;
+							}
+						}
+					}
+					cout << "Bottom to top done. " << queens << " queens" << endl;
+					for (int i = 1; i < 9; i++)//bottom left
+					{
+						if (board.get(file-i, rank-i) == ((board.whiteTurn) ? 5 : 15))
+						{
+							queens++;
+							break;
+						}
+						else if (board.get(file-i, rank-i) != 0)
+						{
+							queens++;
+							break;
+						}
+					}
+					cout << "Bottom left done. " << queens << " queens" << endl;
+					for (int i = 1; i < 9; i++)//bottom right
+					{
+						if (board.get(file + i, rank - i) == ((board.whiteTurn) ? 5 : 15))
+						{
+							queens++;
+							break;
+						}
+						else if (board.get(file+i, rank-i) != 0)
+						{
+							break;
+						}
+					}
+					cout << "Bottom right done. " << queens << " queens" << endl;
+					for (int i = 1; i < 9; i++)//top left
+					{
+						if (board.get(file - i, rank + i) == ((board.whiteTurn) ? 5 : 15))
+						{
+							queens++;
+							break;
+						}
+						else if (board.get(file - i, rank + i) != 0)
+						{
+							break;
+						}
+					}
+					cout << "Top left done. " << queens << " queens" << endl;
+					for (int i = 1; i < 9; i++)//top right
+					{
+						if (board.get(file + i, rank + i) == ((board.whiteTurn) ? 5 : 15))
+						{
+							queens++;
+							break;
+						}
+						else if (board.get(file + i, rank + i) != 0)
+						{
+							break;
+						}
+					}
+					cout << "Top right done. " << queens << " queens" << endl;
+					if(queens>1)
+					{
+						return -1;
+					}
+					else if (queens == 1)
+					{
+						return 1;
+					}
+					break;
+				}
+			case(4) :
+				{
+					int file = move[2] - 1;
+					int rank = move[3] - 1;
+					int rook = false;
+					int block = false;
+					int rooks = 0;
+					cout << "Looking for rooks" << endl;
+					for (int i = 0; i < 9; i++) //left to right check
+					{
+						if (i == file)
+						{
+							rook = false;
+							continue;
+						}
+						if (i < file)
+						{
+							//check for queen and if it has l.o.s to posistion
+							if (board.get(i, rank) == ((board.whiteTurn) ? 4 : 14))
+							{
+								rook = true;
+								rooks++;
+							}
+							else if (board.get(i, rank) != 0 && rook == true)
+							{
+								rook == false;
+								rooks--;
+
+							}
+						}
+						else
+						{
+							if (board.get(i, rank) == ((board.whiteTurn) ? 4 : 14))
+							{
+								rooks++;
+								break;
+							}
+							else if (board.get(i, rank) != 0)
+							{
+								break;
+							}
+						}
+					}//left to right check
+					cout << "Left to right done. " << rooks << " rooks" << endl;
+					for (int i = 0; i < 9; i++) //bottom to top check
+					{
+						if (i == rank)
+						{
+							rook = false;
+							continue;
+						}
+						if (i < rank)
+						{
+							//check for queen and if it has l.o.s to posistion
+							if (board.get(file, i) == ((board.whiteTurn) ? 4 : 14))
+							{
+								rook = true;
+								rooks++;
+							}
+							else if (board.get(file, i) != 0 && rook == true)
+							{
+								rook == false;
+								rooks--;
+
+							}
+						}
+						else
+						{
+							if (board.get(file, i) == ((board.whiteTurn) ? 4 : 14))
+							{
+								rooks++;
+								break;
+							}
+							else if (board.get(file, i) != 0)
+							{
+								break;
+							}
+						}
+					}
+					cout << "Bottom to top done. " << rooks << " rooks" << endl;
+					if (rooks>1)
+					{
+						return -1;
+					}
+					else if (rooks == 1)
+					{
+						return 1;
+					}
+					break;
+				}
+			case(3) :
+			{
+				int file = move[2] - 1;
+				int rank = move[3] - 1;
+				int bishop = false;
+				int block = false;
+				int bishops = 0;
+				cout << "Looking for bishops" << endl;
+				for (int i = 1; i < 9; i++)//bottom left
+				{
+					if (board.get(file - i, rank - i) == ((board.whiteTurn) ? 3 : 13))
+					{
+						bishops++;
+						break;
+					}
+					else if (board.get(file - i, rank - i) != 0)
+					{
+						bishops++;
+						break;
+					}
+				}
+				cout << "Bottom left done. " << bishops << " bishops" << endl;
+				for (int i = 1; i < 9; i++)//bottom right
+				{
+					if (board.get(file + i, rank - i) == ((board.whiteTurn) ? 3 : 13))
+					{
+						bishops++;
+						break;
+					}
+					else if (board.get(file + i, rank - i) != 0)
+					{
+						break;
+					}
+				}
+				cout << "Bottom right done. " << bishops << " bishops" << endl;
+				for (int i = 1; i < 9; i++)//top left
+				{
+					if (board.get(file - i, rank + i) == ((board.whiteTurn) ? 3 : 13))
+					{
+						bishops++;
+						break;
+					}
+					else if (board.get(file - i, rank + i) != 0)
+					{
+						break;
+					}
+				}
+				cout << "Top left done. " << bishops << " bishops" << endl;
+				for (int i = 1; i < 9; i++)//top right
+				{
+					if (board.get(file + i, rank + i) == ((board.whiteTurn) ? 3 : 13))
+					{
+						bishops++;
+						break;
+					}
+					else if (board.get(file + i, rank + i) != 0)
+					{
+						break;
+					}
+				}
+				cout << "Top right done. " << bishops << " bishops" << endl;
+				if (bishops>1)
+				{
+					return -1;
+				}
+				else if (bishops == 1)
+				{
+					return 1;
+				}
+				break;
+			}
+			case(2) :
+			{
+				int file = move[2] - 1;
+				int rank = move[3] - 1;
+				int knights = 0;
+				cout << "Looking for knights" << endl;
+				if (board.get(file + 2, rank - 1) == ((board.whiteTurn) ? 2:12 ))// top left
+				{
+					knights++;
+				}
+				if (board.get(file + 2, rank + 1) == ((board.whiteTurn) ? 2 : 12))// top right
+				{
+					knights++;
+				}
+				if (board.get(file - 2, rank - 1) == ((board.whiteTurn) ? 2 : 12))// bottom left
+				{
+					knights++;
+				}
+				if (board.get(file - 2, rank + 1) == ((board.whiteTurn) ? 2 : 12))// bottom right
+				{
+					knights++;
+				}
+				if (board.get(file + 1, rank - 2) == ((board.whiteTurn) ? 2 : 12))// left top
+				{
+					knights++;
+				}
+				if (board.get(file - 1, rank - 2) == ((board.whiteTurn) ? 2 : 12))// left bottom
+				{
+					knights++;
+				}
+				if (board.get(file + 1, rank + 2) == ((board.whiteTurn) ? 2 : 12))// right top
+				{
+					knights++;
+				}
+				if (board.get(file - 1, rank + 2) == ((board.whiteTurn) ? 2 : 12))// right bottom
+				{
+					knights++;
+				}
+				cout << knights << " knights found" << endl;
+				if (knights>1)
+				{
+					return -1;
+				}
+				else if (knights == 1)
+				{
+					return 1;
+				}
+				break;
+			}
+			case(1) :
+			{
+				int file = move[2] - 1;
+				int rank = move[3] - 1;
+				int pawns = 0;
+				cout << "Looking for pawns" << endl;
+				if (board.whiteTurn)
+				{
+					if (board.get(file, rank - 1) == 1)
+					{
+						pawns++;
+					}
+					if (board.get(file + 1, rank - 1) == 1)
+					{
+						pawns++;
+					}
+					if (board.get(file - 1, rank - 1) == 1)
+					{
+						pawns++;
+					}
+				}
+				else
+				{
+					if (board.get(file, rank + 1) == 11)
+					{
+						pawns++;
+					}
+					if (board.get(file + 1, rank + 1) == 11)
+					{
+						pawns++;
+					}
+					if (board.get(file - 1, rank + 1) == 11)
+					{
+						pawns++;
+					}
+				}
+				cout << pawns << " pawns found" << endl;
+
+				if (pawns>1)
+				{
+					return -1;
+				}
+				else if (pawns == 1)
+				{
+					return 1;
+				}
+				break;
+
+			}
+			}
+			
+		}
+		
+		
+		
+		return false;
+		
+	}
 
 };
 
@@ -140,8 +587,29 @@ public:
 						break;
 					}
 
-					//add ambigious rank/row pawn check (aka a4b5) use convertRankChar, as it will return -1 with lowercase letters
-					if (isRow(input[1])) {
+					if (convertRankChar(input[0]) == -1)
+					{
+						result.push_back(4);
+						result.push_back(1);
+
+						input[0] = toupper(input[0]);
+						int row = (int)input[0] - 64;
+						result.push_back(row);
+
+						string number(1, input[1]);
+						result.push_back(stoi(number));
+
+						input[2] = toupper(input[2]);
+						int row2 = (int)input[2] - 64;
+						result.push_back(row2);
+
+						string number2(1, input[3]);
+						result.push_back(stoi(number2));
+						break;
+
+					}
+
+					if (isRank(input[1])) {
 						result.push_back(2);
 						result.push_back(convertRankChar(input[0]));
 						string number(1, input[1]);
@@ -225,7 +693,7 @@ public:
 		default: return -1;
 		}
 	}
-	bool isRow(char input)
+	bool isRank(char input)
 	{
 		input = toupper(input);
 		if ((int)input < 65)
@@ -237,64 +705,25 @@ public:
 
 Board board;
 Parser parser;
+Inspector inspector;
 
 int main()//possible entries: a4,ba4,ka4,k1a4,kaa4,ka1a4,a8=q,ba8=q,O-O,O-O-O
-{
+ {/*move codes: 
+  -2 castle queenside. 
+  -1 castle kingside. 
+  1 regular move (piece/file/rank). 
+  2 ambigious move, specify rank (piece/origin rank/file/rank). 
+  3 ambigious move, specify file (piece/origin file/file/rank). 
+  4 ambigious move, specify both (piece/origin file/origin rank/file/rank)
+  5 pawn rank-up (upgraded piece/file/rank)
+  6 ambigious pawn rank-up, specify file (upgraded piece/file/file/rank)
+  */
+	board.endTurn();
 	parser.updateBoard(board);
-	vector<int> main = parser.parse("a4");
-	for each (int i in main)
-	{
-		cout << i << " ";
-	}
-	cout << endl;
-	main.clear();
-	main = parser.parse("ba4"); //This is a bug, thinks we are moving bishop, supposed to be pawn in B file moving to A file 4th rank 
-	for each (int i in main) // switching to uppercase letters for piece rank can solve this
-	{
-		cout << i << " ";
-	}
-	cout << endl;
-	main.clear();
-	main = parser.parse("Ka4");
-	for each (int i in main)
-	{
-		cout << i << " ";
-	}
-	cout << endl;
-	main.clear();
-	main = parser.parse("Kba4");
-	for each (int i in main)
-	{
-		cout << i << " ";
-	}
-	cout << endl;
-	main.clear();
-	main = parser.parse("Ka1a4");
-	for each (int i in main)
-	{
-		cout << i << " ";
-	}
-	cout << endl;
-	main.clear();
-	main = parser.parse("exf8=Q");
-	for each (int i in main)
-	{
-		cout << i << " ";
-	}
-	cout << endl;
-	main.clear();
-	main = parser.parse("O-O");
-	for each (int i in main)
-	{
-		cout << i << " ";
-	}
-	cout << endl;
-	main.clear();
-	main = parser.parse("O-O-O");
-	for each (int i in main)
-	{
-		cout << i << " ";
-	}
+	vector<int> processedMove = parser.parse("b2");
+	inspector.updateBoard(board);
+	cout << inspector.isLegal(processedMove) << endl;
+	board.endTurn();
 	
 	system("pause");
     return 0;
